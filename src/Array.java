90001d7776fe92a509582de828684ac63cc1d979
bekzadsarybaev <bekzat97isakov>
2022-09-28 21:06:03 +0600

    commit is done

--- src/Array.java
+++ src/Array.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Array {
+public class Array {
+    private int[] arr = new int[10];
+    private int shetchic = 0;
+
+    public int getSize(){
+        return arr.length;
+    }
+
+    public void add(int arg){
+        if ((shetchic + 1)== arr.length){
+            int[]newArr = new int[Math.round(arr.length*1.5f)];
+            for (int i = 0; i < arr.length; i++) {
+                newArr[i] = arr[i];
+            }
+            arr = newArr;
+        }
+        arr[shetchic] = arg;
+        shetchic++;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0){
+
+            }
+        }
+    }
+    public void remove(int index){
+        for (int i = index; i < arr.length - 1 ; i++) {
+            arr[i]= arr[i+1];
+        }
+        arr[shetchic] = 0;
+        shetchic--;
+    }
+    public int getArr(int index){
+        return arr[index];
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        //int leng = scanner.nextInt();
+        Array arr = new Array();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        arr.add(5);
+        arr.add(6);
+        arr.add(7);
+        arr.add(8);
+        arr.add(9);
+        arr.add(10);
+        arr.add(0);
+        arr.remove(1);
+        for (int i = 0; i < arr.getSize(); i++) {
+            if (arr.getArr(i) > 0){
+                System.out.println(arr.getArr(i));
+            }
+
+        }
     }
 }
